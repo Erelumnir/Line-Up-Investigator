f96b6bf55f48ea736af02a67cde1ed217c6f741c
Randy <36478478+Erelumnir@users.noreply.github.com>
2018-06-01 16:31:01 +0200

    Initial commit

---
+++ Assets/_Game/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Variables
+	public List <GameObject> selectedPerps;
+
+	public void ArrestPerps(){
+		if (selectedPerps != null) {
+			// Destroy all GameObjects in List
+			foreach (GameObject selectedPerp in selectedPerps) {
+				selectedPerps.Remove (selectedPerp);
+				Destroy (selectedPerp, 0.5f);
+			}
+		} else {
+			return;
+		}
+	}
+}
---
+++ Assets/_Game/Scripts/PerpSelection.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerpSelection : MonoBehaviour {
+
+	// Variables
+	[Header("Selection Settings")]
+	public Color hoverColor;
+	public Color selectedColor;
+	private Color startColor;
+
+	private Renderer rend;
+	private bool IsSelected = false;
+
+	[Header("Setup")]
+	public GameManager gameManager;
+
+	void Start(){
+		rend = GetComponent<Renderer> ();
+		startColor = rend.material.color;
+	}
+
+	void OnMouseEnter(){
+		// When hovering over a perp
+		if (!IsSelected) {
+			rend.material.color = hoverColor;
+		}
+	}
+
+	void OnMouseDown(){
+		// When Selecting a perp
+		if (!IsSelected) {
+			AddSelection (this.gameObject);
+		}
+		else if (IsSelected) {
+			RemoveSelection (this.gameObject);
+		}
+	}
+
+	void OnMouseExit(){
+		// Reset color if not selected
+		if (!IsSelected) {
+			rend.material.color = startColor;
+		}
+	}
+
+	void AddSelection(GameObject obj){
+		// Adds Selected GameObject to a list
+		rend.material.color = selectedColor;
+		IsSelected = true;
+		gameManager.selectedPerps.Add(obj);
+		Debug.Log ("GameObject Added");
+	}
+
+	void RemoveSelection(GameObject obj){
+		// Removes Selected GameObject to a list
+		rend.material.color = startColor;
+		IsSelected = false;
+		gameManager.selectedPerps.Remove(obj);
+		Debug.Log ("GameObject Removed");
+	}
+}
