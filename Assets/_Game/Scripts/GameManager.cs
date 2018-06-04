574bb747e6ee018ee6d118e1b9eccfb6265e8411
Randy <36478478+Erelumnir@users.noreply.github.com>
2018-06-04 07:34:55 +0200

    Update 0.2

--- Assets/_Game/Scripts/GameManager.cs
+++ Assets/_Game/Scripts/GameManager.cs
@@ -5,17 +5,50 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
 	// Variables
+	[Header("Game Settings")]
+	// Changes per level
+	public int suspectCounter = 1;
+
+	[Header ("Setup")]
 	public List <GameObject> selectedPerps;
+
+	public PerpAttributes perpA;
+
+	void Update(){
+		// Counter Reaches 0
+		if (suspectCounter <= 0){
+			Win ();
+		}
+	}
 
 	public void ArrestPerps(){
 		if (selectedPerps != null) {
 			// Destroy all GameObjects in List
 			foreach (GameObject selectedPerp in selectedPerps) {
-				selectedPerps.Remove (selectedPerp);
-				Destroy (selectedPerp, 0.5f);
+				perpA = selectedPerps.GetComponents<PerpAttributes> ();
+
+				// Check if the perp is guilty
+				if (perpA.isGuilty) {
+					suspectCounter -= 1;
+					selectedPerps.Remove (selectedPerp);
+					Destroy (selectedPerp, 0.5f);
+				} else if (!perpA.isGuilty) {
+					Lose ();
+				}
 			}
-		} else {
+		} 
+		{
 			return;
 		}
+	}
+
+	void Win(){
+		// WIN
+		Debug.Log("You're promoted!");
+	}
+
+	void Lose(){
+		//LOSE
+		Debug.Log("You're fired!");
 	}
 }
---
+++ Assets/_Game/Scripts/PerpAttributes.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerpAttributes : MonoBehaviour {
+
+	// Variables
+	public string perpName;
+	public bool isGuilty;
+}
